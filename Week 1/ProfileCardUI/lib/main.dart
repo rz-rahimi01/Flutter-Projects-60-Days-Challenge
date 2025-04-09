b712fe24e7d0e6bb23945a5ac6ec34d340618fb9
Rahim Zai Rahimi <152558495+rz-rahimi01@users.noreply.github.com>
2025-04-10 00:21:20 +0500

    Week 1 Day 1

--- Week 1/ProfileCardUI/lib/main.dart
+++ Week 1/ProfileCardUI/lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:profile_card_ui/maincopy.dart';
+import 'package:profile_card_ui/ui.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
-    );
-  }
+  runApp(MyUi());
 }
