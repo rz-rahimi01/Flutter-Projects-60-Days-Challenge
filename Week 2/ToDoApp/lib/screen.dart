84a1e57719c11738b5ffd1a878de26c8fcb16069
Rahim Zai Rahimi <152558495+rz-rahimi01@users.noreply.github.com>
2025-04-20 22:46:37 +0500

    Week 2 Day 5

--- Week 2/ToDoApp/lib/screen.dart
+++ Week 2/ToDoApp/lib/screen.dart
@@ -8,13 +8,18 @@ class Todoapp extends StatefulWidget {
 }
 
 class _TodoappState extends State<Todoapp> {
-  int itemnbr = 0;
   List itemValues = [];
+  List itemdone = [];
+  bool isdone = false;
 
   TextEditingController mycontroller = TextEditingController();
 
   void clearing() {
     mycontroller.clear(); //clears the text in the textfield
+  }
+
+  void checkbox() {
+    setState(() {});
   }
 
   void myprintftn() {
@@ -29,8 +34,8 @@ class _TodoappState extends State<Todoapp> {
       );
     } else {
       setState(() {
-        itemnbr++;
         itemValues.add(mycontroller.text);
+        itemdone.add(false);
         mycontroller.clear();
       });
     }
@@ -47,7 +52,8 @@ class _TodoappState extends State<Todoapp> {
             child: TextField(
               controller: mycontroller,
               minLines: 1, // tell the starting  size of the line
-              maxLines: 2, // tell that the size of the line
+              maxLines:
+                  2, // tell that the textfield can show up to 2 lines and then it will scroll
               keyboardType:
                   TextInputType
                       .text, // tells which keyboard to show while user click on textfield
@@ -76,6 +82,7 @@ class _TodoappState extends State<Todoapp> {
                 borderRadius: BorderRadius.circular(20),
                 side: BorderSide(color: Colors.black),
               ),
+              elevation: 4,
             ),
             label: Text("Add "),
             icon: Icon(Icons.add),
@@ -86,12 +93,24 @@ class _TodoappState extends State<Todoapp> {
               // the benefit of the separated is that it gives a space between the list
               // the listview.separated is used to create a listview with a separator between each item
               separatorBuilder:
-                  (context, index) => Divider(color: Colors.purpleAccent),
+                  (context, index) => Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15),
+                    child: Divider(color: Colors.deepPurple),
+                  ),
               itemCount: itemValues.length,
               itemBuilder: (context, index) {
                 return Dismissible(
                   key: ValueKey(itemValues[index]),
                   background: Container(
+                    color: Colors.green,
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 20),
+                      child: Icon(Icons.archive, color: Colors.white),
+                    ),
+                  ),
+
+                  secondaryBackground: Container(
                     color: Colors.red,
                     alignment: Alignment.centerRight,
                     child: Padding(
@@ -99,13 +118,50 @@ class _TodoappState extends State<Todoapp> {
                       child: Icon(Icons.delete, color: Colors.white),
                     ),
                   ),
-                  direction: DismissDirection.endToStart,
+                  direction: DismissDirection.horizontal,
                   onDismissed: (DismissDirection direction) {
-                    setState(() {
-                      itemValues.removeAt(index);
-                    });
+                    if (direction == DismissDirection.startToEnd) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("Added to archieve"),
+                          duration: Duration(milliseconds: 200),
+                        ),
+                      );
+
+                      //Temproray removing the items to show output as archived
+                      setState(() {
+                        itemValues.removeAt(index);
+                        itemdone.removeAt(index);
+                      });
+                    } else {
+                      setState(() {
+                        itemValues.removeAt(index);
+                        itemdone.removeAt(index);
+                      });
+                    }
                   },
-                  child: ListTile(title: Text(itemValues[index])),
+                  //  child: ListTile(title: Text(itemValues[index])), that was simple listtile now we will use the listtile with the checkbox
+                  child: CheckboxListTile(
+                    title: Text(
+                      itemValues[index],
+                      style: TextStyle(
+                        fontSize: 30,
+                        decoration:
+                            itemdone[index]
+                                ? TextDecoration.lineThrough
+                                : TextDecoration.none,
+                        decorationColor: Colors.red,
+                        decorationThickness: 2,
+                      ),
+                    ),
+                    value: itemdone[index],
+                    onChanged: (bool? newValue) {
+                      setState(() {
+                        itemdone[index] = newValue;
+                      });
+                    },
+                    controlAffinity: ListTileControlAffinity.leading,
+                  ),
                 );
               },
             ),
