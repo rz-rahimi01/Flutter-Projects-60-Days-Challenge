b3c0514707f01f40e081d7404347914e4c163184
Rahim Zai Rahimi <152558495+rz-rahimi01@users.noreply.github.com>
2025-05-05 23:19:58 +0500

    Week 4 Day 2

--- Week 4/FirebaseAuthApp/lib/screens/login_screen.dart
+++ Week 4/FirebaseAuthApp/lib/screens/login_screen.dart
@@ -8,6 +8,18 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController email = TextEditingController();
+  TextEditingController pass = TextEditingController();
+  bool isobscure = true;
+  bool emailError = false;
+  bool passError = false;
+
+  void logCheck() {
+    setState(() {
+      pass.text.isEmpty ? passError = true : passError = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -24,75 +36,113 @@ class _LoginScreenState extends State<LoginScreen> {
           backgroundColor: Colors.deepPurple,
         ),
 
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/login.png'),
-                radius: 100,
-              ),
-              SizedBox(height: 50),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(10),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/login.png'),
+                  radius: 100,
                 ),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.grey[100],
-                        label: Text("Email", style: TextStyle(fontSize: 18)),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Colors.black),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(
-                            color: Colors.deepPurple.shade800,
+                SizedBox(height: 50),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(),
+                  ),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: email,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.grey[100],
+                          label: Text("Email", style: TextStyle(fontSize: 18)),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: Colors.deepPurple.shade800,
+                            ),
                           ),
                         ),
                       ),
-                    ),
 
-                    SizedBox(height: 50),
-
-                    TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.grey[100],
-                        label: Text("Password", style: TextStyle(fontSize: 18)),
-
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Colors.black),
+                      Visibility(
+                        visible: emailError,
+                        child: Text(
+                          "Email is invalid",
+                          style: TextStyle(color: Colors.red),
                         ),
+                      ),
+                      SizedBox(height: 50),
 
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(
-                            color: Colors.deepPurple.shade800,
+                      TextField(
+                        controller: pass,
+                        obscureText: isobscure,
+
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isobscure = !isobscure;
+                              });
+                            },
+                            icon: Icon(
+                              isobscure
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                          ),
+                          filled: true,
+                          fillColor: Colors.grey[100],
+                          label: Text(
+                            "Password",
+                            style: TextStyle(fontSize: 18),
+                          ),
+
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: Colors.deepPurple.shade800,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 50),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        side: BorderSide(color: Colors.black),
+                      Visibility(
+                        visible: passError,
+                        child: Text(
+                          "Password Is Empty",
+                          style: TextStyle(color: Colors.red),
+                        ),
                       ),
-                      child: Text("Login"),
-                    ),
-                  ],
+                      SizedBox(height: 50),
+                      ElevatedButton(
+                        onPressed: () {
+                          logCheck();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          side: BorderSide(color: Colors.black),
+                        ),
+                        child: Text("Login"),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
