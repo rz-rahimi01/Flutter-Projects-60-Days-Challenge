8472307a0b28edec90988191aa3f30bee018c36a
Rahim Zai Rahimi <152558495+rz-rahimi01@users.noreply.github.com>
2025-06-01 22:40:43 +0500

    Week 7 Day 1

--- Week 7/SyntaxHub/lib/main.dart
+++ Week 7/SyntaxHub/lib/main.dart
@@ -1,20 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:syntaxhub/screens/first.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const Start());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Start extends StatelessWidget {
+  const Start({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: First());
   }
 }
---
+++ Week 7/SyntaxHub/lib/screens/first.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class First extends StatefulWidget {
+  const First({super.key});
+
+  @override
+  State<First> createState() => _FirstState();
+}
+
+class _FirstState extends State<First> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
