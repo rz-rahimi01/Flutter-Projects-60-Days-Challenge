2a7ec3c9cb70f8576522f9e76fec095b110bf6a0
Rahim Zai Rahimi <152558495+rz-rahimi01@users.noreply.github.com>
2025-06-03 16:05:15 +0500

    Week 7 Day 2

--- Week 7/SyntaxHub/lib/screens/first.dart
+++ Week 7/SyntaxHub/lib/screens/first.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class First extends StatefulWidget {
   const First({super.key});
@@ -8,8 +9,114 @@ class First extends StatefulWidget {
 }
 
 class _FirstState extends State<First> {
+  List items = [
+    "Python",
+    "Java",
+    "C++",
+    "JavaScript",
+    "Dart",
+    "Ruby",
+    "PHP",
+    "Swift",
+    "Flutter",
+  ];
+
+  List icons = [
+    FontAwesomeIcons.python,
+    FontAwesomeIcons.java,
+    FontAwesomeIcons.cuttlefish,
+    FontAwesomeIcons.js,
+    FontAwesomeIcons.dartLang,
+    FontAwesomeIcons.diamond,
+    FontAwesomeIcons.php,
+    FontAwesomeIcons.swift,
+    FontAwesomeIcons.flutter,
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text("Main Menu"),
+        shadowColor: Colors.black,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(40),
+            bottomRight: Radius.circular(40),
+          ),
+        ),
+        elevation: 3,
+      ),
+      body: Container(
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "All Categories :",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 25),
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, // Creates 3 columns in a vertical grid
+                      mainAxisSpacing: 30, // Spacing between items vertically
+                      crossAxisSpacing:
+                          25.0, // Spacing between items horizontally
+                      childAspectRatio:
+                          1.0, // Ratio of width to height of each item
+                    ),
+                    itemCount:
+                        items.length, // Total number of items in the grid
+                    itemBuilder: (context, index) {
+                      return GridTile(
+                        child: GestureDetector(
+                          onTap: () {
+                            // Handle item tap
+                            print("Tapped on ${items[index]}");
+                          },
+                          child: Card(
+                            elevation: 5,
+                            shadowColor: Colors.black,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  icons[index], // Display the icon for each item
+                                  size: 40,
+                                  color: Colors.blue,
+                                ),
+                                SizedBox(height: 15),
+                                Text(
+                                  items[index], // Display the item name
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
