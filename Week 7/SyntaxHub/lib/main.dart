043a0eb852b24bce660c94c3dce12d499e45a8a5
Rahim Zai Rahimi <152558495+rz-rahimi01@users.noreply.github.com>
2025-06-28 22:33:43 +0500

    nothing special some bugs

--- Week 7/SyntaxHub/lib/main.dart
+++ Week 7/SyntaxHub/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:syntaxhub/screens/splash_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
+  await Hive.initFlutter();
+  await Hive.openBox('bookmarks');
 
   runApp(const Start());
 }
