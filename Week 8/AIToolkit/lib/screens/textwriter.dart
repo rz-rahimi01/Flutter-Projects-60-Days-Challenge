08bbb566bc5c081d91e450a89ce989ab196b6eab
Rahim Zai Rahimi <152558495+rz-rahimi01@users.noreply.github.com>
2025-07-12 21:52:26 +0500

    Week 8 Day 2
    i have exams in that so thats why i am uploading very little means making very less changes

--- Week 8/AIToolkit/lib/screens/textwriter.dart
+++ Week 8/AIToolkit/lib/screens/textwriter.dart
@@ -1,4 +1,5 @@
 import 'package:aitoolkit/widgets/text_dialog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Textwriter extends StatefulWidget {
@@ -9,16 +10,73 @@ class Textwriter extends StatefulWidget {
 }
 
 class _TextwriterState extends State<Textwriter> {
+  late Stream<QuerySnapshot> firebasedata;
+  @override
+  void initState() {
+    super.initState();
+    firebasedata = FirebaseFirestore.instance
+        .collection("app")
+        .doc("data")
+        .collection("textWriter")
+        .snapshots();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.newspaper, size: 40),
-            Text("No data is there", style: TextStyle(fontSize: 20)),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: firebasedata,
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snap) {
+            if (snap.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator(
+                      color: Colors.white,
+                      strokeWidth: 3,
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      "Loading data...",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ],
+                ),
+              );
+            }
+
+            if (snap.hasError) {
+              return Center(child: Text("Error: ${snap.error}"));
+            }
+
+            if (!snap.hasData || snap.data!.docs.isEmpty) {
+              return Center(child: Text("No data available"));
+            }
+
+            return Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ListView.separated(
+                separatorBuilder: (context, index) => Divider(),
+                itemCount: snap.data!.docs.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      snap.data!.docs[index]['userText'] ?? 'No text',
+                    ),
+                    subtitle: Text(
+                      snap.data!.docs[index]['response'] ?? 'No type',
+                    ),
+                  );
+                },
+              ),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
